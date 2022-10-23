cedd68d78f8ef37d89b200b7d3d95b0730d8e083
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-10-23 18:58:56 +0200

    Add nav and style

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {BsFillMoonStarsFill} from 'react-icons/bs';
 
 export default function Home() {
   return (
@@ -11,9 +10,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className=' bg-blue-800 min-h-screen'>
-          Hello
+      <main className=' bg-white px-10'>
+        <section className=' min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-mono'>DiagTech</h1>
+            <ul className=' flex items-center'>
+              <li><BsFillMoonStarsFill className=' cursor-pointer text-2xl'/></li>
+              <li><a className=' bg-orange-600 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
+            </ul>
+          </nav>
+          <div>
+            <h2>Dariusz Jendryczka</h2>
+            <h3>Frontend Developer</h3>
+            <p>Freelancer providing services in frontend.</p>
+          </div>
         </section>
       </main>
 
